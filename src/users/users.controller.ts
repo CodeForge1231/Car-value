e1346610622ee5bb9d972e8949d495dc10127efa
Andriy Sozanskiy <sozanskiyandriyj@gmail.com>
2024-12-31 15:10:30 +0200

    Add serialization and security for user data responses

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,8 @@ import {
   Query,
   Delete,
   NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dtos';
 import { UsersService } from './users.service';
@@ -20,6 +22,7 @@ export class UsersController {
     this.userService.create(body.email, body.password);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.userService.findOne(parseInt(id));
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,6 +14,7 @@ export class UsersService {
   findOne(id: number) {
     return this.repo.findOneBy({ id });
   }
+
   find(email: string) {
     return this.repo.find({ where: { email } });
   }
