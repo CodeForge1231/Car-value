4df506e93ed056c29cf920d324b57f721c1d4ee8
Andriy Sozanskiy <sozanskiyandriyj@gmail.com>
2025-01-07 15:01:39 +0200

    Way 1: Apply globally scoped pipe for request validation

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,7 @@ const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['asdasdd'],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
