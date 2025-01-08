ad093a6a80e1bb7e4e9ac67f324e45f8c9f52a64
Andriy Sozanskiy <sozanskiyandriyj@gmail.com>
2025-01-08 23:43:45 +0200

    Implement GetEstimate functionality using createQueryBuilder with refined filtering and approval check

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -3,7 +3,7 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Report } from './reports.entity';
 import { CreateReportDto } from './dtos/create-report.dto';
-
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 import { User } from '../users/users.entity';
 @Injectable()
 export class ReportsService {
@@ -22,5 +22,21 @@ export class ReportsService {
 
     report.approved = approved;
     return this.repo.save(report);
+  }
+
+  createEstimate(estimateDto: GetEstimateDto) {
+    return this.repo
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make= :make', { make: estimateDto.make })
+      .andWhere('model = :model', { model: estimateDto.model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng: estimateDto.lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat: estimateDto.lat })
+      .andWhere('year - :year BETWEEN -5 AND 5', { year: estimateDto.year })
+      .andWhere('approved IS TRUE')
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
+      .setParameters({ mileage: estimateDto.mileage })
+      .limit(3)
+      .getRawOne();
   }
 }
