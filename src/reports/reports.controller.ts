5fa3868be6975c33655fbd5d265d3e8def2d7224
Andriy Sozanskiy <sozanskiyandriyj@gmail.com>
2025-01-08 21:59:26 +0200

    Add AdminGuard, CurrentUser Middleware, and Admin Field to User Entity

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -5,15 +5,19 @@ import {
   UseGuards,
   Patch,
   Param,
+  Get,
+  Query,
 } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
+import { AdminGuard } from 'src/guards/admin.guard';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from '../users/users.entity';
 import { ReportDto } from './dtos/report.dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
+
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
@@ -24,6 +28,7 @@ export class ReportsController {
     return this.reportsService.create(body, user);
   }
 
+  @UseGuards(AdminGuard)
   @Patch('/:id')
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
