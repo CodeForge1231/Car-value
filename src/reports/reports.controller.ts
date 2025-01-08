7d4378e56d4d6677a971e971e01edd24c3e97e09
Andriy Sozanskiy <sozanskiyandriyj@gmail.com>
2025-01-08 22:23:23 +0200

    Add GetEstimateDto and Endpoint for Querying Estimates

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -17,6 +17,7 @@ import { User } from '../users/users.entity';
 import { ReportDto } from './dtos/report.dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -33,4 +34,7 @@ export class ReportsController {
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
   }
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {}
 }
